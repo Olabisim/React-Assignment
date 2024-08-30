79e19cd3bb568f445653813b207b27626f71e5e2
Olabisim <olabisiajoseh@gmail.com>
2024-08-30 09:28:03 +0100

    added prime react

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import 'primereact/resources/primereact.min.css';
+import 'primereact/resources/themes/lara-light-teal/theme.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
