81ce17c764d7691019009cf7ff8d76d33c857cf1
Olabisim <olabisiajoseh@gmail.com>
2024-08-30 15:36:03 +0100

    added overlaypanel to select multiple rows

--- src/components/DatatableComponent.tsx
+++ src/components/DatatableComponent.tsx
@@ -1,12 +1,15 @@
 
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { InputSwitch } from 'primereact/inputswitch';
 import useSWR from "swr";
 import { Paginator } from 'primereact/paginator';
 import { ProgressSpinner } from 'primereact/progressspinner';
+import { OverlayPanel } from 'primereact/overlaypanel';
+import { Button } from 'primereact/button';
+import { InputNumber } from 'primereact/inputnumber';
 
 
 interface onPageEventInterface {
@@ -24,6 +27,8 @@ export const DataTableComponent = () => {
     const [first, setFirst] = useState<number>(0);
     const [rows, setRows] = useState<number>(10);
     const [currentPage, setCurrentPage] = useState<number>(0);
+    const [value, setValue] = useState(0);
+    const op:any = useRef(null);
 
     const fetcher = (...args : [any]) => fetch(...args).then(res => res.json())
     const { data, isLoading } = useSWR(`https://api.artic.edu/api/v1/artworks?page=${currentPage}`, fetcher)
@@ -41,10 +46,20 @@ export const DataTableComponent = () => {
         setRows(event.rows);
     };
 
+    const handleMultiSelect = (e:any) => op.current.toggle(e)
 
     return (
         <>
-            
+            {/* <Button type="button" icon="pi pi-image" label="Image" onClick={(e:any) => op.current.toggle(e)} /> */}
+            <OverlayPanel ref={op}>
+                <label htmlFor="selectNumber" className="font-bold block mb-2">Select rows</label>
+                <div>
+                    <InputNumber inputId="selectNumber" value={value} onValueChange={(e:any) => setValue(e.value)} minFractionDigits={0} maxFractionDigits={0} min={0} max={10506} />
+                </div>
+                <Button type="button" label="submit" severity="secondary" onClick={handleMultiSelect} />
+
+            </OverlayPanel>
+
             <div className="card">
                 <div className="flex justify-content-center align-items-center mb-4 gap-2">
                     <InputSwitch inputId="input-rowclick" checked={rowClick} onChange={(e) => setRowClick(e.value)} />
@@ -61,6 +76,7 @@ export const DataTableComponent = () => {
                     {/* <DataTable value={products} selectionMode={rowClick ? null : 'checkbox'} selection={selectedProducts} onSelectionChange={(e) => setSelectedProducts(e.value)} dataKey="id" tableStyle={{ minWidth: '50rem' }}> */}
                     
                         <Column selectionMode="multiple" headerStyle={{ width: '3rem' }}></Column>
+                        <Column header={<span className="pi pi-chevron-down" onClick={(e:any) => op.current.toggle(e)}></span>}></Column>
                         <Column field="title" header="Title"></Column>
                         <Column field="place_of_origin" header="Place of Origin"></Column>
                         <Column field="artist_display" header="Artist Display"></Column>
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import 'primeicons/primeicons.css';
 import 'primereact/resources/primereact.min.css';
 import 'primereact/resources/themes/lara-light-teal/theme.css';
 
